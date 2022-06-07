1af6d05d3ea06be51283a2c8a9ce03f4f46567c4
RedstoneRuler <earthstar67876@gmail.com>
2022-06-07 17:13:25 -0400

    Update Alphabet.hx

--- source/Alphabet.hx
+++ source/Alphabet.hx
@@ -222,12 +222,13 @@ class Alphabet extends FlxSpriteGroup
 	{
 		if (isMenuItem)
 		{
+			
 			var scaledY = FlxMath.remapToRange(targetY, 0, 1, 0, 1.3);
 
 			y = FlxMath.lerp(y, (scaledY * 120) + (FlxG.height * 0.48), 0.16);
 			x = FlxMath.lerp(x, (targetY * 20) + 90, 0.16);
 		}
-
+		setGraphicSize(Std.int(width) + FlxG.random.int(-1, 1));
 		super.update(elapsed);
 	}
 }
